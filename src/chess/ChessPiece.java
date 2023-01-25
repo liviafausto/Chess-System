ffdb48d918c7ada56304f1503f9e2f9241ba1dd9
Livia <liviafausto03@gmail.com>
2023-01-25 17:50:20 -0300

    fix: removed unnecessary constructors

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,7 +6,6 @@ import boardgame.Piece;
 public class ChessPiece extends Piece {
     private Color color;
 
-    public ChessPiece(){}
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
