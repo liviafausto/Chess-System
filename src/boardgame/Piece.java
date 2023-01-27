be526854df03e6a6560de81f05642c79434fca69
Livia <liviafausto03@gmail.com>
2023-01-27 15:56:05 -0300

    feat: starting to implement possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -11,5 +11,25 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] moves = possibleMoves();
+
+        for(int i=0; i<moves.length; i++){
+            for(int j=0; j<moves.length; j++){
+                if(moves[i][j]){
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
