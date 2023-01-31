b69f8390ad7154acb19954fb7d8a3eddee15818c
Livia <liviafausto03@gmail.com>
2023-01-31 14:29:44 -0300

    feat: implemented Queen's moves

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -16,6 +16,33 @@ public class Queen extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves(){
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        // The Queen can move in all directions - in its rows, columns or diagonally
+        boolean[][] queensMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        // 1. Moves above
+        considerMoveTowards(queensMoves, -1, 0);
+
+        // 2. Moves below
+        considerMoveTowards(queensMoves, +1, 0);
+
+        // 3. Moves to the left
+        considerMoveTowards(queensMoves, 0, -1);
+
+        // 4. Moves to the right
+        considerMoveTowards(queensMoves, 0, +1);
+
+        // 5. Moves towards the northwest
+        considerMoveTowards(queensMoves, -1, -1);
+
+        // 6. Moves towards the northeast
+        considerMoveTowards(queensMoves, -1, +1);
+
+        // 7. Moves towards the southwest
+        considerMoveTowards(queensMoves, +1, -1);
+
+        // 8. Moves towards the southeast
+        considerMoveTowards(queensMoves, +1, +1);
+
+        return queensMoves;
     }
 }
